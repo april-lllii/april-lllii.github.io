d4bea998b6b1d83ac284618a114949e1bb19c1a5
April <lzq20030404@gmail.com>
2024-05-10 02:28:58 +0100

    update project pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kite': ['"Kite One"', 'sans-serif'],
+        'kiwi': ['"Kiwi Maru"', 'serif']
+      },
+      backgroundImage: {
+        'gradient-to-b-3-5': 'linear-gradient(to bottom, #B0D7F6 60%, #F7DCDC 40%)' // Replace colors as needed
+      }
+    }
   },
   plugins: [],
 }
